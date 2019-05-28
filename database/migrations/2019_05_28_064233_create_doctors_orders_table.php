7e0cc56aa42df737a281c1798c81de2b751f7403
vagrant <vagrant@laravelspa>
2019-05-28 06:57:23 +0000

    doctors order model

---
+++ database/migrations/2019_05_28_064233_create_doctors_orders_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors_orders', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->integer('patient_record_id')->unsigned()->nullable();
+            $table->foreign('patient_record_id')->references('id')->on('patient_records')->onDelete('cascade');
+            
+            $table->date('date')->nullable();
+            $table->time('time')->nullable();
+
+            $table->string('progress_note')->nullable();
+
+            $table->string('orders')->nullable();
+
+            $table->jsonb('laboratories')->nullable();
+
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors_orders');
+    }
+}
