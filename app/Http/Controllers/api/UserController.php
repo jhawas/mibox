a4a884b241a0403dedfda6f4ba829ecca55a7f33
Norman Arañez <13890@Flatworld-PH.com>
2019-06-11 15:06:55 +0800

    removed fullname in user controller

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -57,7 +57,6 @@ class UserController extends Controller
         $user->first_name = $request->first_name;
         $user->middle_name = $request->middle_name;
         $user->last_name = $request->last_name;
-        $patient->full_name = $request->first_name + ' ' + $request->middle_name + ' ' + $request->last_name;
         $user->username = $request->username;
         $user->suffix = $request->suffix;
         $user->email = $request->email;
