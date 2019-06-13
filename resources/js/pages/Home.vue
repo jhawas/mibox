96f0909e29900adfe09558176db6da47c1657735
Norman Arañez <13890@Flatworld-PH.com>
2019-06-13 15:41:00 +0800

    dashboard update

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -14,7 +14,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
             <div class="info">
-              <h4>Users</h4>
+              <h4>Patient</h4>
               <p><b>5</b></p>
             </div>
           </div>
@@ -22,7 +22,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
             <div class="info">
-              <h4>Likes</h4>
+              <h4>In Patient</h4>
               <p><b>25</b></p>
             </div>
           </div>
@@ -30,7 +30,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
             <div class="info">
-              <h4>Uploades</h4>
+              <h4>Rooms</h4>
               <p><b>10</b></p>
             </div>
           </div>
@@ -38,7 +38,7 @@
         <div class="col-md-6 col-lg-3">
           <div class="widget-small danger coloured-icon"><i class="icon fa fa-star fa-3x"></i>
             <div class="info">
-              <h4>Stars</h4>
+              <h4>Laboratories</h4>
               <p><b>500</b></p>
             </div>
           </div>
