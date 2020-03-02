7dae31ff34ba3225462bb8c8f13aa496aa7a2d28
Norman Arañez <13890@Flatworld-PH.com>
2020-03-02 14:09:34 +0800

    removed star of a journey in all pages

--- resources/js/pages/billings/Home.vue
+++ resources/js/pages/billings/Home.vue
@@ -3,13 +3,12 @@
         <div class="app-title">
             <div>
                 <h1><i class="fa fa-dashboard"></i> Billing Page</h1>
-                  <p>Start a beautiful journey here</p>
-                </div>
-                <ul class="app-breadcrumb breadcrumb">
-                  <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-                  <li class="breadcrumb-item"><a href="#">Billing Page</a></li>
-                </ul>
             </div>
+            <ul class="app-breadcrumb breadcrumb">
+              <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+              <li class="breadcrumb-item"><a href="#">Billing Page</a></li>
+            </ul>
+          </div>
           <div class="row">
             <div class="col-md-12">
               <div class="tile">
--- resources/js/pages/patientRecords/Home.vue
+++ resources/js/pages/patientRecords/Home.vue
@@ -3,7 +3,7 @@
         <div class="app-title">
             <div>
                 <h1><i class="fa fa-dashboard"></i> In Patient Page</h1>
-                  <p>Start a beautiful journey here</p>
+                  
                 </div>
                 <ul class="app-breadcrumb breadcrumb">
                   <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
